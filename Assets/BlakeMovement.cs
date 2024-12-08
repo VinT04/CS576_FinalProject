2a74256062e85cae480fb84962edf3b76f033c85
Vin Tyagi <tyagivinamra@gmail.com>
2024-12-08 10:24:07 -0500

    re-add cinemachine
    guess who's back? Back again?

--- Assets/BlakeMovement.cs
+++ Assets/BlakeMovement.cs
@@ -5,12 +5,13 @@ using System.Threading;
 using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using Cinemachine;
 
 public class BlakeMovement:MonoBehaviour
 {
-    public Camera Camera;
-    public Quaternion targetRotation;
     public float velocity_forward = 0f;
+    public Cinemachine3rdPersonFollow Camera;
+    public GameObject FollowTarget;
     public Vector3 movement_direction;
     private Animator animation_controller;
     private CharacterController character_controller;
@@ -18,6 +19,8 @@ public class BlakeMovement:MonoBehaviour
     public float jumpTarget = 10f;
     public float gravity = 25.0f;
     public float speed = 1f;
+    public bool isRotatingLeft = false;
+    public bool isRotatingRight = false;
 
     // Start is called before the first frame update
     void Start()
@@ -40,17 +43,13 @@ public class BlakeMovement:MonoBehaviour
         {
             if (Input.GetKey(KeyCode.W))
             {
-                transform.rotation = Camera.transform.rotation;
                 if (Input.GetKey(KeyCode.LeftShift))
                 {
                     if (Input.GetKey(KeyCode.Space))
                     {
                         // Jump
-                        if (transform.position.y == 0f)
-                        {
-                            moveDirection.y += 10f;
-                            animation_controller.Play("jump start");
-                        }
+                        moveDirection.y += 10f;
+                        animation_controller.Play("jump start");
                     }
                     else
                     {
@@ -75,26 +74,40 @@ public class BlakeMovement:MonoBehaviour
         }
 
         // Handle the rotation of the character
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            Camera.transform.RotateAround(transform.position, Vector3.up, -100f * Time.deltaTime);
+            isRotatingLeft = true;
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
         {
-            Camera.transform.RotateAround(transform.position, Vector3.up, 100f * Time.deltaTime);
+            isRotatingLeft = false;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            isRotatingRight = true;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            isRotatingRight = false;
+        }
+        if (isRotatingLeft)
+        {
+            transform.Rotate(0, -200f * Time.deltaTime, 0);
+        }
+        if (isRotatingRight)
+        {
+            transform.Rotate(0, 200f * Time.deltaTime, 0);
         }
 
         // Handle movement of character and camera follow
         character_controller.Move(moveDirection * Time.deltaTime);
-        Camera.transform.position = Camera.transform.position + moveDirection * Time.deltaTime;
         if (transform.position.y > 0f)
         {
             moveDirection.y -= gravity * Time.deltaTime;
         }
-        if (transform.position.y < 0f || Camera.transform.position.y < 1.68f)
+        if (transform.position.y < 0f)
         {
             transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
-            Camera.transform.position = new Vector3(Camera.transform.position.x, 1.68f, Camera.transform.position.z);
         }
     }
 }
