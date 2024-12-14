fb86c78f5d78b59fae1b38513f5527ea45b79e91
aarav-nair <aarav.nair@gmail.com>
2024-12-14 18:01:30 -0500

    Rebase

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -18,68 +18,80 @@ public class Tutorial : MonoBehaviour
         "Avoid being caught by the mummy\nPress any key to continue",
         "Now find your way to the pyramid to begin"
     };
-    private int index;
+    private int index = 0;
+    private float timer = 0.0f;
+    private float time_limit = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        index = 0;
         tutorial_text.text = popups[index];
-        //tutorial_runner();
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool ctrl = Input.GetKey(KeyCode.C);
+        timer += Time.deltaTime;
+
         bool any = Input.anyKey;
-        bool shift = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-        bool up = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W);
-        bool down = Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S);
-        bool left = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
-        bool right = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
-        bool space = Input.GetKey(KeyCode.Space);
+        bool shift = Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        bool up = Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W);
+        bool down = Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S);
+        bool left = Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A);
+        bool right = Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D);
+        bool space = Input.GetKeyDown(KeyCode.Space);
 
-        tutorial_text.text = popups[index];
+        if (timer > time_limit)
+        {
+            if (index == 0 && any)
+            {
+                next_prompt();
+                time_limit = 5.0f;
+            }
+            else if (index == 1 && up)
+            {
+                next_prompt();
+            }
+            else if (index == 2 && down)
+            {
+                next_prompt();
+            }
+            else if (index == 3 && left)
+            {
+                next_prompt();
+            }
+            else if (index == 4 && right)
+            {
+                next_prompt();
+            }
+            else if (index == 5 && up && shift)
+            {
+                next_prompt();
+            }
+            else if (index == 6 && up && shift && space)
+            {
+                next_prompt();
+            }
+            else if (index > 6 && index < 9 && any)
+            {
+                next_prompt();
+            }
+        }
+    }
 
-        if (index == 0 && any)
+    private void next_prompt()
+    {
+        next_prompt();
+        if (index < popups.Length)
         {
-
-            index += 1;
+            tutorial_text.text = popups[index];
         }
-        else if (index == 1 && up)
-        {
-            index += 1;
-        }
-        else if (index == 2 && down)
-        {
-            index += 1;
-        }
-        else if (index == 3 && left)
-        {
-            index += 1;
-        }
-        else if (index == 4 && right)
-        {
-            index += 1;
-        }
-        else if (index == 5 && up && shift)
-        {
-            index += 1;
-        }
-        else if (index == 6 && up && shift && space)
-        {
-            index += 1;
-        }
-        else if (index > 6 && index < 9 && any)
-        {
-            index += 1;
-        }
+        timer -= time_limit;
     }
 
     IEnumerator tutorial_runner()
     {
-        bool ctrl = Input.GetKey(KeyCode.C);
+        bool ctrl = Input.GetKeyDown(KeyCode.C);
         bool any = Input.anyKey;
         bool shift = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
         bool up = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W);
